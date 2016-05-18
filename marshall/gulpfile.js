a17b4dc0d7a2f4d4dd202af72eaeab1e96689508
Mars <marshallmiller92@gmail.com>
2016-05-18 16:22:24 -0700

    gulp watching

--- marshall/gulpfile.js
+++ marshall/gulpfile.js
@@ -2,13 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-eslint');
 var watch = require('gulp-watch');
 
-gulp.task('link', function() {
+gulp.task('default', ['lint', 'test'], function(){
+    console.log('GULP LIVES!!!')
+});
+
+gulp.task('lint', function() {
  return gulp.src(['./greet.js', '!node_modules/gulp-eslint'])
   .pipe(eslint());
   .pipe(eslint.format());
   .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint'], function(){
-  //???
-});
+gulp.watch('./**/*.js', ['lint', 'test']);
